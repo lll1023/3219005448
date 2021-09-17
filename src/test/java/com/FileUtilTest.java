4d0ca6a3d2074e74207a649335f427c0102a73b7
github <2335287299@qq.com>
2021-09-17 21:45:05 +0800

    修改测试用例

--- src/test/java/com/FileUtilTest.java
+++ src/test/java/com/FileUtilTest.java
@@ -2,6 +2,8 @@ package com;
 
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+
 /**
  * @Author: Lsutin
  * @Date: 2021/9/16 10:50
@@ -36,8 +38,15 @@ public class FileUtilTest {
     public void testContext1(){
         String file1 = "D:\\课内学习\\软件工程\\orig.txt";
         String file2 = "D:\\课内学习\\软件工程\\orig_0.8_add.txt";
-        SimHash simHash1 = new SimHash(StringUtil.processFile(file1));
-        SimHash simHash2 = new SimHash(StringUtil.processFile(file2));
+        SimHash simHash1;
+        SimHash simHash2;
+        try {
+            simHash1 = new SimHash(StringUtil.processFile(file1));
+            simHash2 = new SimHash(StringUtil.processFile(file2));
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+            return;
+        }
         String similarity = simHash1.getSimilarity(simHash2);
         StringBuilder builder = new StringBuilder();
         builder.append("原文文件：").append(file1).append("\n");
@@ -51,8 +60,15 @@ public class FileUtilTest {
     public void testContext2(){
         String file1 = "D:\\课内学习\\软件工程\\orig.txt";
         String file2 = "D:\\课内学习\\软件工程\\orig_0.8_del.txt";
-        SimHash simHash1 = new SimHash(StringUtil.processFile(file1));
-        SimHash simHash2 = new SimHash(StringUtil.processFile(file2));
+        SimHash simHash1;
+        SimHash simHash2;
+        try {
+            simHash1 = new SimHash(StringUtil.processFile(file1));
+            simHash2 = new SimHash(StringUtil.processFile(file2));
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+            return;
+        }
         String similarity = simHash1.getSimilarity(simHash2);
         StringBuilder builder = new StringBuilder();
         builder.append("原文文件：").append(file1).append("\n");
@@ -66,8 +82,15 @@ public class FileUtilTest {
     public void testContext3(){
         String file1 = "D:\\课内学习\\软件工程\\orig.txt";
         String file2 = "D:\\课内学习\\软件工程\\orig_0.8_dis_1.txt";
-        SimHash simHash1 = new SimHash(StringUtil.processFile(file1));
-        SimHash simHash2 = new SimHash(StringUtil.processFile(file2));
+        SimHash simHash1;
+        SimHash simHash2;
+        try {
+            simHash1 = new SimHash(StringUtil.processFile(file1));
+            simHash2 = new SimHash(StringUtil.processFile(file2));
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+            return;
+        }
         String similarity = simHash1.getSimilarity(simHash2);
         StringBuilder builder = new StringBuilder();
         builder.append("原文文件：").append(file1).append("\n");
@@ -81,8 +104,15 @@ public class FileUtilTest {
     public void testContext4(){
         String file1 = "D:\\课内学习\\软件工程\\orig.txt";
         String file2 = "D:\\课内学习\\软件工程\\orig_0.8_dis_10.txt";
-        SimHash simHash1 = new SimHash(StringUtil.processFile(file1));
-        SimHash simHash2 = new SimHash(StringUtil.processFile(file2));
+        SimHash simHash1;
+        SimHash simHash2;
+        try {
+            simHash1 = new SimHash(StringUtil.processFile(file1));
+            simHash2 = new SimHash(StringUtil.processFile(file2));
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+            return;
+        }
         String similarity = simHash1.getSimilarity(simHash2);
         StringBuilder builder = new StringBuilder();
         builder.append("原文文件：").append(file1).append("\n");
@@ -96,8 +126,15 @@ public class FileUtilTest {
     public void testContext5(){
         String file1 = "D:\\课内学习\\软件工程\\orig.txt";
         String file2 = "D:\\课内学习\\软件工程\\orig_0.8_dis_15.txt";
-        SimHash simHash1 = new SimHash(StringUtil.processFile(file1));
-        SimHash simHash2 = new SimHash(StringUtil.processFile(file2));
+        SimHash simHash1;
+        SimHash simHash2;
+        try {
+            simHash1 = new SimHash(StringUtil.processFile(file1));
+            simHash2 = new SimHash(StringUtil.processFile(file2));
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+            return;
+        }
         String similarity = simHash1.getSimilarity(simHash2);
         StringBuilder builder = new StringBuilder();
         builder.append("原文文件：").append(file1).append("\n");
@@ -111,8 +148,15 @@ public class FileUtilTest {
     public void testContext7(){
         String file1 = "D:\\课内学习\\软件工程\\orig_0.8_dis_1.txt";
         String file2 = "D:\\课内学习\\软件工程\\测试文本\\orig_0.8_dis_1.txt";
-        String context1 = StringUtil.processFile(file1);
-        String context2 = StringUtil.processFile(file2);
+        String context1="";
+        String context2="";
+        try {
+            context1 = StringUtil.processFile(file1);
+            context2 = StringUtil.processFile(file2);
+        }catch (FileNotFoundException e){
+            System.out.println();
+            return;
+        }
         System.out.println(context1);
         System.out.println(context2);
         System.out.println(context1.equals(context2));
@@ -123,8 +167,15 @@ public class FileUtilTest {
     public void testContext6(){
         String file1 = "D:\\课内学习\\软件工程\\测试文本\\orig.txt";
         String file2 = "D:\\课内学习\\软件工程\\测试文本\\orig_0.8_dis_1.txt";
-        SimHash simHash1 = new SimHash(StringUtil.processFile(file1));
-        SimHash simHash2 = new SimHash(StringUtil.processFile(file2));
+        SimHash simHash1;
+        SimHash simHash2;
+        try {
+            simHash1 = new SimHash(StringUtil.processFile(file1));
+            simHash2 = new SimHash(StringUtil.processFile(file2));
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+            return;
+        }
         String similarity = simHash1.getSimilarity(simHash2);
         StringBuilder builder = new StringBuilder();
         builder.append("原文文件：").append(file1).append("\n");
