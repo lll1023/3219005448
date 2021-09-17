31cbb62e2c71e78a504e61ea19346639b21ac995
github <2335287299@qq.com>
2021-09-17 21:16:26 +0800

    重新修改提取html的文本的逻辑

--- src/main/java/com/StringUtil.java
+++ src/main/java/com/StringUtil.java
@@ -14,26 +14,38 @@ import java.util.regex.Pattern;
 public class StringUtil {
 
     private static String delHTML(String context){
-        Pattern htmlPattern = Pattern.compile(".*\\<[^>]+>.*",Pattern.DOTALL);
-        boolean isHTML = htmlPattern.matcher(context).matches();
-        if(!isHTML){
-            return context;
-        }
         StringBuilder builder = new StringBuilder();
-        //解析html内容
-        Document doc = Jsoup.parse(context);
-        //获取tr标签中的td标签内容
-        //这个是观察文本中的html格式后，找出的针对这类html的解析
-        Elements trs = doc.select("table").select("tr");
-        for(int i = 0;i<trs.size();i++){
-            Elements tds = trs.get(i).select("td");
-            for(int j = 0;j<tds.size();j++){
-                String text = tds.get(j).text();
-                builder.append(text);
+        String[] strings = {"[\\pP\\p{Punct}]"," "};
+        try {
+            Pattern htmlPattern = Pattern.compile(".*\\<[^>]+>.*",Pattern.DOTALL);
+            boolean isHTML = htmlPattern.matcher(context).matches();
+            if(!isHTML){
+                for (String string:strings){
+                    context = context.replaceAll(string,"");
+                }
+                return context;
             }
+            //解析html内容
+            Document doc = Jsoup.parse(context);
+            //获取tr标签中的td标签内容
+            //这个是观察文本中的html格式后，找出的针对这类html的解析
+            Elements trs = doc.select("table").select("tr");
+            for(int i = 0;i<trs.size();i++){
+                Elements tds = trs.get(i).select("td");
+                for(int j = 0;j<tds.size();j++){
+                    String text = tds.get(j).text();
+                    builder.append(text);
+                }
+            }
+            context = builder.toString();
+            for (String string:strings){
+                context = context.replaceAll(string,"");
+            }
+        }catch (NullPointerException e){
+
         }
 
-        return builder.toString();
+        return context;
     }
 
     public static String processFile(String file){
