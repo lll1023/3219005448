642553ecb9b17f7319faba069d63b1fcd970d6b4
github <2335287299@qq.com>
2021-09-18 09:35:12 +0800

    修改测试用例

--- src/test/java/com/FileUtilTest.java
+++ src/test/java/com/FileUtilTest.java
@@ -38,21 +38,8 @@ public class FileUtilTest {
     public void testContext1(){
         String file1 = "D:\\课内学习\\软件工程\\orig.txt";
         String file2 = "D:\\课内学习\\软件工程\\orig_0.8_add.txt";
-        SimHash simHash1;
-        SimHash simHash2;
-        try {
-            simHash1 = new SimHash(StringUtil.processFile(file1));
-            simHash2 = new SimHash(StringUtil.processFile(file2));
-        }catch (FileNotFoundException e){
-            System.out.println(e.getMessage());
-            return;
-        }
-        String similarity = simHash1.getSimilarity(simHash2);
-        StringBuilder builder = new StringBuilder();
-        builder.append("原文文件：").append(file1).append("\n");
-        builder.append("抄袭版论文的文件：").append(file2).append("\n");
-        builder.append("两个文件的相似度为：").append(similarity).append("\n");
-        System.out.println(builder.toString());
+        String file3 = "D:\\课内学习\\软件工程\\target.txt";
+        new MainProcess().main(new String[]{file1,file2,file3});
     }
 
     //5.测试文本相似度
@@ -60,21 +47,8 @@ public class FileUtilTest {
     public void testContext2(){
         String file1 = "D:\\课内学习\\软件工程\\orig.txt";
         String file2 = "D:\\课内学习\\软件工程\\orig_0.8_del.txt";
-        SimHash simHash1;
-        SimHash simHash2;
-        try {
-            simHash1 = new SimHash(StringUtil.processFile(file1));
-            simHash2 = new SimHash(StringUtil.processFile(file2));
-        }catch (FileNotFoundException e){
-            System.out.println(e.getMessage());
-            return;
-        }
-        String similarity = simHash1.getSimilarity(simHash2);
-        StringBuilder builder = new StringBuilder();
-        builder.append("原文文件：").append(file1).append("\n");
-        builder.append("抄袭版论文的文件：").append(file2).append("\n");
-        builder.append("两个文件的相似度为：").append(similarity).append("\n");
-        System.out.println(builder.toString());
+        String file3 = "D:\\课内学习\\软件工程\\target.txt";
+        new MainProcess().main(new String[]{file1,file2,file3});
     }
 
     //6.测试文本相似度
@@ -82,21 +56,8 @@ public class FileUtilTest {
     public void testContext3(){
         String file1 = "D:\\课内学习\\软件工程\\orig.txt";
         String file2 = "D:\\课内学习\\软件工程\\orig_0.8_dis_1.txt";
-        SimHash simHash1;
-        SimHash simHash2;
-        try {
-            simHash1 = new SimHash(StringUtil.processFile(file1));
-            simHash2 = new SimHash(StringUtil.processFile(file2));
-        }catch (FileNotFoundException e){
-            System.out.println(e.getMessage());
-            return;
-        }
-        String similarity = simHash1.getSimilarity(simHash2);
-        StringBuilder builder = new StringBuilder();
-        builder.append("原文文件：").append(file1).append("\n");
-        builder.append("抄袭版论文的文件：").append(file2).append("\n");
-        builder.append("两个文件的相似度为：").append(similarity).append("\n");
-        System.out.println(builder.toString());
+        String file3 = "D:\\课内学习\\软件工程\\target.txt";
+        new MainProcess().main(new String[]{file1,file2,file3});
     }
 
     //7.测试文本相似度
@@ -104,21 +65,8 @@ public class FileUtilTest {
     public void testContext4(){
         String file1 = "D:\\课内学习\\软件工程\\orig.txt";
         String file2 = "D:\\课内学习\\软件工程\\orig_0.8_dis_10.txt";
-        SimHash simHash1;
-        SimHash simHash2;
-        try {
-            simHash1 = new SimHash(StringUtil.processFile(file1));
-            simHash2 = new SimHash(StringUtil.processFile(file2));
-        }catch (FileNotFoundException e){
-            System.out.println(e.getMessage());
-            return;
-        }
-        String similarity = simHash1.getSimilarity(simHash2);
-        StringBuilder builder = new StringBuilder();
-        builder.append("原文文件：").append(file1).append("\n");
-        builder.append("抄袭版论文的文件：").append(file2).append("\n");
-        builder.append("两个文件的相似度为：").append(similarity).append("\n");
-        System.out.println(builder.toString());
+        String file3 = "D:\\课内学习\\软件工程\\target.txt";
+        new MainProcess().main(new String[]{file1,file2,file3});
     }
 
     //8.测试文本相似度
@@ -126,21 +74,8 @@ public class FileUtilTest {
     public void testContext5(){
         String file1 = "D:\\课内学习\\软件工程\\orig.txt";
         String file2 = "D:\\课内学习\\软件工程\\orig_0.8_dis_15.txt";
-        SimHash simHash1;
-        SimHash simHash2;
-        try {
-            simHash1 = new SimHash(StringUtil.processFile(file1));
-            simHash2 = new SimHash(StringUtil.processFile(file2));
-        }catch (FileNotFoundException e){
-            System.out.println(e.getMessage());
-            return;
-        }
-        String similarity = simHash1.getSimilarity(simHash2);
-        StringBuilder builder = new StringBuilder();
-        builder.append("原文文件：").append(file1).append("\n");
-        builder.append("抄袭版论文的文件：").append(file2).append("\n");
-        builder.append("两个文件的相似度为：").append(similarity).append("\n");
-        System.out.println(builder.toString());
+        String file3 = "D:\\课内学习\\软件工程\\target.txt";
+        new MainProcess().main(new String[]{file1,file2,file3});
     }
 
     //9.在html中提取文本,比较提取和没提取的文本内容（不包含标点符号以及空格）
@@ -157,8 +92,6 @@ public class FileUtilTest {
             System.out.println();
             return;
         }
-        System.out.println(context1);
-        System.out.println(context2);
         System.out.println(context1.equals(context2));
     }
 
@@ -167,21 +100,8 @@ public class FileUtilTest {
     public void testContext6(){
         String file1 = "D:\\课内学习\\软件工程\\测试文本\\orig.txt";
         String file2 = "D:\\课内学习\\软件工程\\测试文本\\orig_0.8_dis_1.txt";
-        SimHash simHash1;
-        SimHash simHash2;
-        try {
-            simHash1 = new SimHash(StringUtil.processFile(file1));
-            simHash2 = new SimHash(StringUtil.processFile(file2));
-        }catch (FileNotFoundException e){
-            System.out.println(e.getMessage());
-            return;
-        }
-        String similarity = simHash1.getSimilarity(simHash2);
-        StringBuilder builder = new StringBuilder();
-        builder.append("原文文件：").append(file1).append("\n");
-        builder.append("抄袭版论文的文件：").append(file2).append("\n");
-        builder.append("两个文件的相似度为：").append(similarity).append("\n");
-        System.out.println(builder.toString());
+        String file3 = "D:\\课内学习\\软件工程\\target.txt";
+        new MainProcess().main(new String[]{file1,file2,file3});
     }
 
 }
